0d62bb45c48f0d3d0215bb75b20d09c3ce0707d3
RaelZero <gaz.gong@gmail.com>
2014-05-13 12:33:30 +0200

    Button positioning

--- index.php
+++ index.php
@@ -9,11 +9,12 @@
     <body>
         <header>
             <span>PinFood</span>
+            <button>Search</button>
+            <button>Log In</button>
             <button>IT</button>
             <button>EN</button>
-            <button>Log In</button>
-            <button>Search</button>
         </header>
+        
         <div id="mapframe">
             <iframe id="map"
                 frameborder="0" style="border:0"
@@ -21,6 +22,7 @@
                 &q=Venezia,Italy">
             </iframe>
         </div>
-        <footer>Blank as well</footer>
+        
+        <footer>Made in Trento, TESTWAFFE 2014</footer>
     </body>
 </html>
