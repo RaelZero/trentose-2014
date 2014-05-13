dec1f94606a992e089199030e63fdf8a58b262d2
Jujo920 <jenny920@hotmail.it>
2014-05-13 14:45:37 +0200

    Immagini, posizionamento

--- index.php
+++ index.php
@@ -9,16 +9,15 @@
     <body>
         <header>
             <span>PinFood</span>
-            <button><img src="http://icons.iconarchive.com/icons/visualpharm/icons8-metro-style/512/Very-Basic-Search-icon.png"/></button>
-            <button ID="login">Login</button>
-            <button><img src="https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcRuSrkS0Yv1sVIazMs1wQZCkEGmUEar1FvT_p-F2SblG7-SSU9K1QhbXg"/></button>
-            <button><img src="http://www.gruppoitalcem.it/wp-content/uploads/2010/03/flag_icon_gb.gif"/></button>
+            <button><img src="img/magnifierglass.svg" width="auto" height="30px"></button>
+            <button id="login">Login</button>
+            <button><img src="img/it.svg" width="50px" height="auto" /></button>
+            <button><img src="img/uk.svg" width="50px" height="auto" /></button>
         <div id="mapframe">
             <iframe id="map"
                 frameborder="0" style="border:0"
                 src="https://www.google.com/maps/embed/v1/place?key=AIzaSyCgOGVYe_2bYYuYCn4DMdRwWpS950k9cME
-                &q=Venezia,Italy" 
-            </iframe>
+                &q=Venezia,Italy"></iframe>
         </div>
         
         <footer>Made in Trento, TESTWAFFE 2014</footer>
