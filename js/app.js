fff0cbfc97330f42ae577d50a681ce59b23b87e8
RaelZero <gaz.gong@gmail.com>
2014-05-27 12:02:00 +0200

    Pin adding

--- js/app.js
+++ js/app.js
@@ -1,12 +1,18 @@
 var markerMode = false;
+// Global only because I'm lazy
+var map = null;
 
 $(document).ready (function() {
+    console.log("Conditions could hardly be more ideal!");
+    
     // Start by hiding the overlay from the main window
     $(".overlay").hide();
     
     // Render the map using geolocation, if availible
     if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(PinFoodMap.loadMap);        
+        // On success, calls back to PinFoodMap.loadMap
+        navigator.geolocation.getCurrentPosition(PinFoodMap.loadMap);
+        console.log(map);
     }
     else {
       alert('geolocation not supported');
@@ -21,6 +27,7 @@ $(document).ready (function() {
     $(".add").click(function( ) {
         markerMode = !markerMode;
         
+        /*
         if(markerMode){
             $(".popup_add").show();
             $(".overlay").show();
@@ -32,6 +39,7 @@ $(document).ready (function() {
             $(".overlay").hide();
             $(".add").css('z-index', '0');
         }
+        */
     });
     
     $(".search").click(function( ) {
@@ -51,12 +59,27 @@ $(document).ready (function() {
         
         $("div[class*='popup_']").hide();
     });
+    
+    var mapOverlay = new google.maps.OverlayView();
+    mapOverlay.draw = function() {};
+    mapOverlay.setMap(map);
+    
+    
+    /*
+    // Click event catcher for the map
+    google.maps.event.addListener(PinFoodMap.map, 'click', function(){
+        if(markerMode){
+            map newPin = overlay.getProjection().fromContainerPixelToLatLng(new google.maps.Point(x, y));
+            var newMarker = new google.maps.Marker(position: newPin, map : PinFoodMap.map);
+        }
+    });
+    
+    */
 
 });
 
 
 PinFoodMap = {
-
     loadMap : function (position) {
      
         // Position conversion
@@ -71,16 +94,17 @@ PinFoodMap = {
             mapTypeId: google.maps.MapTypeId.ROADMAP
         };
         
-        PinFoodMap.map = new google.maps.Map(document.getElementById("map"), myOptions);
+        map = new google.maps.Map(document.getElementById("map"), myOptions);
+        console.log(map);
     
-        var myPos = new google.maps.LatLng(46.066145, 11.150349);
+        var povo = new google.maps.LatLng(46.066145, 11.150349);
 
         var marker = new google.maps.Marker({
-            position : myPos,
+            position : povo,
             title : "Marker gnurante"
         });
 
-        marker.setMap(PinFoodMap.map);
+        marker.setMap(map);
     },
   
     addSampleMarker: function (pos) {
