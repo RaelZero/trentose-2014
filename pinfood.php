910ccf0f0124e7bff036ed9c504bd566b2a4fcdb
RaelZero <gaz.gong@gmail.com>
2014-05-27 12:40:48 +0200

    Pins fetching

--- pinfood.php
+++ pinfood.php
@@ -1,15 +1,14 @@
 <?php
 
-function db_connect()
-{
+function db_connect(){
     $host = "localhost";
     $dbname = "trentose";
     $user = "trentose";
     $pass = "CYQhjxVc6JF5LY5c";
 
     $connection = mysql_connect($host,$user,$pass);
-    echo("Now connected to DB.\n");
-    echo($connection);
+    echo ("Now connected to DB." . PHP_EOL);
+    echo (PHP_EOL . $connection);
     return $connection;
 }
 
@@ -17,62 +16,70 @@ function db_connect()
 function pin_load (){
     $PIN_LOAD_SQL = "select * from trentose.places";
     
-	
-//         open database
-        $dbh = db_connect();   
-	
-        $fp = fopen('pins.json', 'w+');
+    //open database
+    $dbh = db_connect();
+
+    $fp = fopen("./res/pins.json", "w+");
+    
 	$sth = mysql_query($PIN_LOAD_SQL);
+    
 	$rows = array();
-	while($r = mysql_fetch_assoc($sth)) 
-	{
-    		$rows[] = $r;
-		
-	}
+	while($r = mysql_fetch_assoc($sth)){
+    	$rows[] = $r;
+    }
 
-	echo json_encode($rows);
+	//echo json_encode($rows);
 
+	fwrite($fp, json_encode($rows));
+    fclose($fp);
+    echo("Finished writing on JSON file, now availible at pins.json\n");
 	
-	
-        fwrite($fp, json_encode($rows));
-        fclose($fp);
-        echo("Finished writing on JSON file, now availible at pins.json\n");
-	
-        // close database
-	
-        $dbh = null;
+    // close database
+	$dbh = null;
     	
     
 }
-/*
+// funzione per l'inserimento del pin
+// @PARAM array associativo contenente 'nome' 'address' 'picture' 'street' 'city' 'state' 'country' 'latitude' 'longitude'
 function pin_insert ($place){
-    $PIN_INSERT_SQL = "INSERT INTO places(name, address, lat, lng)".
-                      " values (:place_name, :place_address, :place_lat, :place_lng)";
+    $PIN_INSERT_SQL = "INSERT INTO trentose.places(name,description,picture,loc_street,loc_city,loc_state,loc_country,loc_latitude, loc_longitude)".
+                      " values (:place_name, :place_description, :place_picture, :place_loc_street, :place_loc_city, :place_loc_state,:place_loc_country, :place_loc_latitude, :place_loc_longitude)";
     
     try {
         // open database
         $dbh = db_connect(); 
-        
+
         $stmt = $dbh->prepare($PIN_INSERT_SQL); 
-        
-        $stmt->bindValue(':place_name', $place['name']);        
-        $stmt->bindValue(':place_address', $place['address']);
-        $stmt->bindValue(':place_lat', $place['lat']);
-        $stmt->bindValue(':place_lng', $place['lng']);                
-        
-        $stmt->execute();        
-        
+
+        $stmt->bindValue(':place_name', $place['name']);
+        $stmt->bindValue(':place_description', $place['address']);
+        $stmt->bindValue(':place_picture', $place['picture']);
+        $stmt->bindValue(':place_loc_street', $place['street']);
+	$stmt->bindValue(':place_loc_city', $place['city']);
+	$stmt->bindValue(':place_loc_state', $place['state']);
+	$stmt->bindValue(':place_loc_country', $place['country']);
+	$stmt->bindValue(':place_loc_latitude', $place['latitude']);
+	$stmt->bindValue(':place_loc_longitude', $place['longitude']);
+
+        $stmt->execute();
+
         // close database
         $dbh = null;
-        
+
     }
     catch(PDOException $e){
         echo $e->getMessage();
-    } 
-} 
-  
-*/
+    }
+}
+
+
+function calculate_users_score($user,)
+{
+	
+}
+
+
 
 pin_load();
- 
+
 ?>
